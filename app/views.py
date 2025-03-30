00f3d87cb8ee23daee3c5ecc7ff98217d5d01063
niggumin <minecrafffft.4lyf@gmail.com>
2025-03-31 00:04:42 +0300

    nasledovanie

--- app/views.py
+++ app/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 def index(request):
     # return HttpResponse("Hello world")
-    return render(request, 'base.html')
+    return render(request, 'signup.html')
