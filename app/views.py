8a4e233ba5c6a878c32a4497f40730378636e1d7
niggumin <minecrafffft.4lyf@gmail.com>
2025-04-01 17:00:31 +0300

    pum pum

--- app/views.py
+++ app/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 
+questions = []
+for i in range(1,30):
+    questions.append({ 
+                    'title': 'title' + str(i),
+                    'author': "Krasnoglazik",
+                    'id': i,
+                    'text': 'text' + str(i),
+                    'tag1' : 'Tag №3',
+                    'tag2' : 'Tag №2',
+                    })
+
 def index(request):
-    # return HttpResponse("Hello world")
+    return render(request, 'index.html', context={'questions': questions})
+
+def hot(request):
+    return render(request, 'hot.html', context={'questions': questions})
+
+def tag(request):
+    return render(request, 'tag.html', context={'questions': questions})
+
+def question(request):
+    return render(request, 'question.html', context={'questions': questions})
+
+def login(request):
+    return render(request, 'login.html')
+
+def signup(request):
     return render(request, 'signup.html')
+
+def ask(request):
+    return render(request, 'ask.html')
--- askme_vavilov/urls.py
+++ askme_vavilov/urls.py
@@ -21,5 +21,11 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('hot', views.hot, name='hot'),
+    path('tag/<slug:tag>/', views.tag, name='tag'),
+    path('question/<int:id>/', views.question, name='question'),
+    path('login', views.login, name='login'),
+    path('signup', views.signup, name='signup'),
+    path('ask', views.ask, name='ask'),
 ]
