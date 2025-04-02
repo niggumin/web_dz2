f2edde775b90d1fcc7cc96462b3cdcb249a388c0
niggumin <minecrafffft.4lyf@gmail.com>
2025-04-02 14:47:47 +0300

    bebra

--- app/views.py
+++ app/views.py
@@ -1,30 +1,45 @@
+import copy
 from django.shortcuts import render
-# from django.http import HttpResponse
+from django.http import HttpResponse
 
 # Create your views here.
 
 questions = []
-for i in range(1,30):
+for i in range(0,30):
     questions.append({ 
-                    'title': 'title' + str(i),
+                    'title': 'Question number ' + str(i),
                     'author': "Krasnoglazik",
                     'id': i,
-                    'text': 'text' + str(i),
-                    'tag1' : 'Tag №3',
-                    'tag2' : 'Tag №2',
+                    'text': '#ТЕКСТ ' + str(i),
+                    'tag1' : 'Tag3',
+                    'tag2' : 'Tag2',
+                    })
+
+answers = []
+for i in range(0,30):
+    answers.append({ 
+                    'author': "AmNyam",
+                    'id': i,
+                    'text': '#ТЕКСТ I really do not know what answer will be more suitable for the situation which you have described and even can not give you even a simple advice about this complex topic.' + str(i),
                     })
 
 def index(request):
     return render(request, 'index.html', context={'questions': questions})
 
 def hot(request):
-    return render(request, 'hot.html', context={'questions': questions})
+    snoitseuq = reversed(copy.deepcopy(questions))
+    return render(request, 'hot.html', context={'questions': snoitseuq})
 
-def tag(request):
-    return render(request, 'tag.html', context={'questions': questions})
+def tag(request, targetTag):
+    
+    return render(request, 'tag.html', context={'questions': questions, 'targetTag': targetTag})
 
-def question(request):
-    return render(request, 'question.html', context={'questions': questions})
+def question(request, questionId):
+   
+    if questionId == -1:
+        return HttpResponse("WRONG ID")
+    
+    return render(request, 'question.html', context={'question': questions[questionId], 'answers': answers})
 
 def login(request):
     return render(request, 'login.html')
